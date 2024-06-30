f8651da3c18debb8a8ca44ad0ca8423140668d77
joaocastro <joaocastro0429@gmail.com>
2024-06-29 23:38:25 -0300

    create params

--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ const express =require('express')
 
 const app=express()
 
-app.get("/",(req,res)=>{
+app.get("/user",(req,res)=>{
     res.send("Minha primeira api ")
 
+})
+
+app.get("/user/:name",(req,res)=>{
+    const name=req.params.name
+    res.json({name})
 })
 
 
